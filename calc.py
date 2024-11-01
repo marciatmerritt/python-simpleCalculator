9d59aee2279c362f1178d8a2db1242845e06d7c6
marciatmerritt <121736033+marciatmerritt@users.noreply.github.com>
2024-10-31 22:38:08 -0400

    Create calc.py
    Start with the calc

---
+++ calc.py
@@ -0,0 +1,33 @@
+def add(x, y):
+  return x + y
+
+def subtract(x, y):
+  return x - y
+
+def multiply(x, y):
+  return x * y
+
+def divide(x, y):
+  if y != 0:
+    return x / y
+  else:
+    print("Division by zero not allowed!)
+
+print("Select operation: ")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+  # get input from user
+  choice = input("Enter Selection: ")
+  if choice in ('1', '2', '3', '4'):
+    try:
+      # get numbers from the user
+      num1 = float(input("Enter first number: "))
+      num2 = float(input("Enter second number: "))
+    except
+      
+  else:
+    print("Invalid Input. Try Again")
